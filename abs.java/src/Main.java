336ca5f5971fd95eeba324316ea01165806265d0
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-03 18:37:27 +0530

    Simple class

--- abs.java/src/Main.java
+++ abs.java/src/Main.java
@@ -7,7 +7,7 @@ class abs{
     public static void main(String[] args) {
         A obj=new A(){
             public void show(){
-                System.out.println("My age is 22");
+                System.out.println("My age is 94");
             }
             public void age(){
                 System.out.println("Hi");
