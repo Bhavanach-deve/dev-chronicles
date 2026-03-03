8440c8b93bdfc5bd0dfa3c3523c839586b5ebc67
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-03 18:47:06 +0530

    How about a Calender Date?

--- LaunchDateandTime/src/LaunchNewDateAndTimeAPI.java
+++ LaunchDateandTime/src/LaunchNewDateAndTimeAPI.java
@@ -9,13 +9,13 @@ public class LaunchNewDateAndTimeAPI
         LocalDate date=LocalDate.now();
         System.out.println(date);
         System.out.println(date.getDayOfYear());
-        System.out.println("***********************");
+        System.out.println("*****************************");
         LocalDate dob=LocalDate.of(2002,06,24);
         System.out.println(dob);
-        System.out.println("***************************");
+        System.out.println("******************************");
         LocalTime time=LocalTime.now();
         System.out.println(time);
-        System.out.println("***************************");
+        System.out.println("*******************************");
         LocalDateTime timeDate= LocalDateTime.now();
         System.out.println(timeDate);
 
