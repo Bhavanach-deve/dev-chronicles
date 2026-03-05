e28d939e6136b2ad81dc366ec1dc62347c7be0ee
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-05 16:36:53 +0530

    Add leap year checker program

---
+++ DSA/src/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LeapYear
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a year to check if it is a leap year.");
+        int a = sc.nextInt();
+        if(a%4==0 && a%100!=0){
+            System.out.println("Nice! You've got a leap year.");
+        } else
+        {
+            System.out.println("The entered year is not a leap year.");
+        }
+    }
+}
+//To check if a year is leap year the year should be perfectly divisible by 4 and "should not" divisible by 100 or Should be divisible by 400.
