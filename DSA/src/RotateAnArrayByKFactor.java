c8a42f03a9596b0faa902a4ac68a9b626586ea4b
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-07 18:09:29 +0530

    Add program to rotate an array by k positions

---
+++ DSA/src/RotateAnArrayByKFactor.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class RotateAnArrayByKFactor
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter no.of elements:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the number by which the array has to be rotated:");
+        int k = sc.nextInt();
+
+        // rotate k times
+        for(int j = 0; j < k; j++){
+
+            int first = arr[0];
+
+            for(int i = 0; i < n - 1; i++){
+                arr[i] = arr[i + 1];
+            }
+
+            arr[n - 1] = first;
+        }
+
+        System.out.println("Array after rotation:");
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
