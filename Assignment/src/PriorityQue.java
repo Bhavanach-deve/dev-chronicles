ad66abf5c980e195d7e8d2dc80d428fd46998305
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-17 17:40:25 +0530

    Basic program on priority queue Collection

---
+++ Assignment/src/PriorityQue.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class PriorityQue
+{
+    public static void main(String[] args)
+    {
+        PriorityQueue p=new PriorityQueue();
+        p.add(50);
+        p.add(60);
+        p.add(140);
+        p.add(90);
+        p.add(10);
+        p.add(45);
+        p.add(1);
+        System.out.println(p);
+
+    }
+}
